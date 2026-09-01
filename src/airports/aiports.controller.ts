a36db3a51af60df139dbe9fcfab75e762a6070c5
Bil-exe <nabilsamoura.0@gmail.com>
2026-09-01 02:58:13 +0200

    add routes controller get departuresByAirport

--- src/airports/aiports.controller.ts
+++ src/airports/aiports.controller.ts
@@ -1,17 +1,29 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
-import { get } from 'axios';
 import { AirportsService } from './airports.service';
-import e from 'express';
+import { AxiosResponse } from 'axios';
+import { error } from 'console';
 
 @Controller('airports')
 export class AiportsController {
-    constructor(private readonly airportService: AirportsService) {};
+    constructor(private readonly airportService: AirportsService) { };
     @Get('arrivals')
-    // @Query pour les parametre de requete : après un ? 
-    getArrivals(@Query('icao') icao:string, @Query('begin') begin:number, @Query('end') end:number){
-        console.log('Query Parameter from get Arrival : ', icao,begin,end);
-        return this.airportService.getArrivalsByAirport(icao,begin,end);
+    // @Query pour les parametre de requete : après un ? et
+    // exemple de parametre : icao = EDDF begin = 1517227200 end = 1517230800
+    getArrivals(@Query('icao') icao: string, @Query('begin') begin: number, @Query('end') end: number): Promise<AxiosResponse> {
+        console.log('Query Parameter from get Arrival : ', icao, begin, end);
+        return this.airportService.getArrivalsByAirport(icao, begin, end);
     }
 
+    @Get('departures')
+    getDepartures(@Query('icao') icao: string, @Query('begin') begin: number, @Query('end') end: number): Promise<AxiosResponse> {
+        try {
 
+            console.log('Query parameter from get departure : ', icao, begin, end);
+            return this.airportService.getDepartureByAirport(icao, begin, end);
+
+        } catch (err: any) {
+
+            throw new error('Problem in controller Get departures : ', err)
+        }
+    }
 }
--- src/airports/airports.service.ts
+++ src/airports/airports.service.ts
@@ -3,14 +3,15 @@ import { Injectable } from '@nestjs/common';
 import { AxiosResponse } from 'axios';
 import { firstValueFrom } from 'rxjs';
 import { OpenSkyService } from '../opensky/opensky.service';
+import { error } from 'console';
 
 @Injectable()
 export class AirportsService {
     constructor(private readonly httpService: HttpService, private readonly openSkyService: OpenSkyService) { };
-    async getArrivalsByAirport(icao: string, begin: number, end: number): Promise<any> {
+    async getArrivalsByAirport(icao: string, begin: number, end: number): Promise<AxiosResponse> {
         try {
-            const token: string =  await this.openSkyService.getToken();
-            const response = await firstValueFrom(
+            const token: string = await this.openSkyService.getToken();
+            const response: AxiosResponse = await firstValueFrom(
                 this.httpService.get(`
                 https://opensky-network.org/api/flights/arrival?airport=${icao}&begin=${begin}&end=${end}`,
                     {
@@ -21,14 +22,35 @@ export class AirportsService {
 
                 )
             )
-            
+
             console.log('icao :', icao)
             console.log('begin :', begin)
             console.log('end :', end)
-            console.log('token in airport service ', token);
+            // console.log('token in airport service ', token);
             return response.data;
         } catch (err: any) {
-            console.error('Error GetArrivalsByAirports : ', err);
+            throw new error('Error GetArrivalsByAirports : ', err);
+        }
+
+    }
+    async getDepartureByAirport(icao: string, begin: number, end: number): Promise<AxiosResponse> {
+        const token: string = await this.openSkyService.getToken();
+        try {
+            const response: AxiosResponse = await firstValueFrom(
+                this.httpService.get(
+                    `https://opensky-network.org/api/flights/departure?airport=${icao}&begin=${begin}&end=${end}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        }
+                    }
+                )
+            )
+            console.log('Query Parameter : ', icao, begin, end);
+            return response.data;
+
+        } catch (err: any) {
+            throw new error('Problem in getDepartureByAirport : ', err);
         }
 
     }
