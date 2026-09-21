2aa1bfd0122c5713e7feba7642accea8186cb9f1
Bil-exe <nabilsamoura.0@gmail.com>
2026-09-21 02:24:38 +0200

    add method findAll

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -3,5 +3,5 @@ import { CreateUserDto } from './create-user.dto';
 
 //PartialType créé une copie de CreateUserDto cependant chaques champs est optionnel pour permettrre la modification de seulement une seule ressource 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-
+ 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) { }
+
+  // Créer un user 
+  async create(createUserDto: CreateUserDto) {
+    try {
+      // Verify if the user already exist
+      const existingUser = await this.prisma.user.findUnique({
+        where: { email: createUserDto.email }
+      })
+      console.log('FindUnique email : ', existingUser);
+
+      if (existingUser) {
+        throw new ConflictException('This email is already taken by an another user')
+      }
+
+      // hash the password if the email is good
+      const hashedPassword: string = await bcrypt.hash(createUserDto.password, 10);
+      console.log('HashedPassword :', hashedPassword);
+
+      //create the user in database 
+      const user = await this.prisma.user.create({
+        data: {
+          ...createUserDto, // Spread all fields
+          password: hashedPassword // replace by the hashed version
+        },
+        select: { // only return the fiels you want to see not the password 
+          id: true,
+          name: true,
+          email: true,
+          password: false,
+          createdAt: true,
+          updatedAt: true,
+          role: true
+        }
+
+      });
+
+      return user;
+
+    } catch (err: any) {
+      throw new Error('Can\'t create the user : ', err);
+    }
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    try {
+      const users = await this.prisma.user.findMany({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          password: false,
+          createdAt: true,
+          updatedAt: true,
+          role: true
+        }
+      })
+      return users;
+
+    } catch (err: any) {
+
+      throw new Error('Can\'t return alls users, maybe check the database connexion : ', err)
+    }
+    // return `This action returns all users`;
   }
 
   findOne(id: number) {
